07358188401183670451b2c3d3e768cc3985d8a3
Michał Mocek <pekinhere@gmail.com>
2021-12-09 20:10:47 +0100

    various changes

--- src/main/java/com/sfg/gateway/config/LoadBalancerRoutes.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancerRoutes.java
@@ -14,21 +14,14 @@ public class LoadBalancerRoutes {
     public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                       .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                                   .uri("lb://beer-service")
-                                   .id("beer-service"))
-                      .route(r -> r.path("/api/v1/customers/**", "/api/v1/customers*")
-                                   .uri("lb://beer-order-service")
-                                   .id("beer-order-service"))
+                                   .uri("lb://beer-service"))
+                      .route(r -> r.path("/api/v1/customers/**", "/api/v1/customers*").uri("lb://beer-order-service"))
                       .route(r -> r.path("/api/v1/beer/*/inventory*")
                                    .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
                                                                         .setFallbackUri("forward:/inventory-failover")
-                                                                        .setRouteId("inventory-failover")
-                                   ))
-                                   .uri("lb://beer-inventory-service")
-                                   .id("beer-inventory-service"))
-                      .route(r -> r.path("/inventory-failover/**")
-                                   .uri("lb://inventory-failover")
-                                   .id("inventory-failover-service"))
+                                                                        .setRouteId("inventory-failover")))
+                                   .uri("lb://beer-inventory-service"))
+                      .route(r -> r.path("/inventory-failover/**").uri("lb://inventory-failover"))
                       .build();
     }
 }
